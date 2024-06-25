a63fb984486213b05b2f5ff7b3fabf2f94b45f54
Дамир Барашев <barashev-01@mail.ru>
2024-06-25 15:02:53 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -17,56 +17,56 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		return
 	}
-
-	conn, err := l.Accept()
-	reader := bufio.NewReader(conn)
-	fmt.Println(conn.LocalAddr())
-	requestLine, err := reader.ReadString('\n')
-
-	if err != nil {
-		fmt.Println("Error reading request:", err)
-		return
-	}
-
-	headers := make(map[string]string)
 	for {
-		line, err := reader.ReadString('\n')
+		conn, err := l.Accept()
+		reader := bufio.NewReader(conn)
+		fmt.Println(conn.LocalAddr())
+		requestLine, err := reader.ReadString('\n')
+
 		if err != nil {
-			fmt.Println("Error reading header line:", err)
+			fmt.Println("Error reading request:", err)
 			return
 		}
-		line = strings.TrimSpace(line)
-		if line == "" {
-			break
+
+		headers := make(map[string]string)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading header line:", err)
+				return
+			}
+			line = strings.TrimSpace(line)
+			if line == "" {
+				break
+			}
+			headerParts := strings.SplitN(line, ": ", 2)
+			if len(headerParts) == 2 {
+				headers[headerParts[0]] = headerParts[1]
+			}
 		}
-		headerParts := strings.SplitN(line, ": ", 2)
-		if len(headerParts) == 2 {
-			headers[headerParts[0]] = headerParts[1]
+
+		requestLine = strings.TrimSpace(requestLine)
+		parts := strings.Split(requestLine, " ")
+		parts = strings.Split(parts[1], "/")
+		target := parts[1]
+		switch target {
+		case "user-agent":
+			UserAgent := headers["User-Agent"]
+			conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", len(UserAgent)) + "\r\n\r\n" + UserAgent))
+		case "echo":
+			echo := parts[2]
+			EchoLen := len(echo)
+			conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", EchoLen) + "\r\n\r\n" + echo))
+		case "":
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		default:
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+
+		fmt.Println("Received request: ", requestLine)
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
 	}
-
-	requestLine = strings.TrimSpace(requestLine)
-	parts := strings.Split(requestLine, " ")
-	parts = strings.Split(parts[1], "/")
-	target := parts[1]
-	switch target {
-	case "user-agent":
-		UserAgent := headers["User-Agent"]
-		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", len(UserAgent)) + "\r\n\r\n" + UserAgent))
-	case "echo":
-		echo := parts[2]
-		EchoLen := len(echo)
-		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", EchoLen) + "\r\n\r\n" + echo))
-	case "":
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	default:
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	}
-
-	fmt.Println("Received request: ", requestLine)
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
 }
