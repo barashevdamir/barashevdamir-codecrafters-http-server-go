2c2dc1db8b852e3520e8dc56a6ba50c9d59d8f1f
Дамир Барашев <barashev-01@mail.ru>
2024-06-25 10:52:45 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,27 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", ":4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		return
 	}
 
 	conn, err := l.Accept()
+	reader := bufio.NewReader(conn)
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading request:", err)
+		return
+	}
+	requestLine = strings.TrimSpace(requestLine)
+	parts := strings.Split(requestLine, " ")
+	target := parts[1]
+	if target == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
 }
